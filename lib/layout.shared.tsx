9546e8220e7a3bf4df5e13db899b8b3e9d44e270
Kickblip <wyatt.zilker@gmail.com>
2026-08-11 12:05:27 -0500

    init

--- lib/layout.shared.tsx
+++ lib/layout.shared.tsx
@@ -1,11 +1,19 @@
-import type { BaseLayoutProps } from 'fumadocs-ui/layouts/shared';
-import { appName, gitConfig } from './shared';
+import type { BaseLayoutProps } from "fumadocs-ui/layouts/shared";
+import { appName, gitConfig } from "./shared";
+import { Logo } from "@/components/logo";
 
 export function baseOptions(): BaseLayoutProps {
   return {
     nav: {
       // JSX supported
-      title: appName,
+      title: (
+        <>
+          <Logo size={32} />
+          <span className="uppercase text-xs px-2 py-0.5 font-semibold border rounded-full border-yellow-600 bg-yellow-700 text-yellow-100">
+            Developers
+          </span>
+        </>
+      ),
     },
     githubUrl: `https://github.com/${gitConfig.user}/${gitConfig.repo}`,
   };
