1f2779ffae2ccddee8fff91e5b40a3716bcde6fe
Kickblip <wyatt.zilker@gmail.com>
2026-09-05 15:42:32 -0500

    Add prettier formatting

--- lib/layout.shared.tsx
+++ lib/layout.shared.tsx
@@ -1,6 +1,6 @@
-import type { BaseLayoutProps } from "fumadocs-ui/layouts/shared";
-import { appName, gitConfig } from "./shared";
-import { Logo } from "@/components/logo";
+import type { BaseLayoutProps } from "fumadocs-ui/layouts/shared"
+import { appName, gitConfig } from "./shared"
+import { Logo } from "@/components/logo"
 
 export function baseOptions(): BaseLayoutProps {
   return {
@@ -9,12 +9,12 @@ export function baseOptions(): BaseLayoutProps {
       title: (
         <>
           <Logo size={32} />
-          <span className="uppercase text-xs px-2 py-0.5 font-semibold border rounded-full border-yellow-600 bg-yellow-700 text-yellow-100">
+          <span className="rounded-full border-blue-800 bg-blue-500 px-2 py-0.5 text-xs font-semibold text-white uppercase">
             Developers
           </span>
         </>
       ),
     },
     githubUrl: `https://github.com/${gitConfig.user}/${gitConfig.repo}`,
-  };
+  }
 }
