acbfc8cde8512d86d635ebf5d1b582d588d014e8
Kickblip <wyatt.zilker@gmail.com>
2026-09-10 10:32:13 -0500

    New documentation pages

--- lib/layout.shared.tsx
+++ lib/layout.shared.tsx
@@ -8,10 +8,11 @@ export function baseOptions(): BaseLayoutProps {
       // JSX supported
       title: (
         <>
-          <Logo size={32} />
-          <span className="rounded-full border-blue-800 bg-blue-500 px-2 py-0.5 text-xs font-semibold text-white uppercase">
-            Developers
-          </span>
+          <Logo size={24} />
+          <p className="text-sm font-medium">
+            <span>blueward</span>
+            <span className="opacity-70">.dev</span>
+          </p>
         </>
       ),
     },
