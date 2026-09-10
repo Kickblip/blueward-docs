15ba37f843e19e444b92d059df0c7794f532c6c2
Kickblip <wyatt.zilker@gmail.com>
2026-09-10 12:30:48 -0500

    New dropdown menu selector for docs pages

--- app/(docs)/layout.tsx
+++ app/(docs)/layout.tsx
@@ -4,7 +4,25 @@ import { baseOptions } from "@/lib/layout.shared"
 
 export default function Layout({ children }: LayoutProps<"/">) {
   return (
-    <DocsLayout tree={source.getPageTree()} {...baseOptions()}>
+    <DocsLayout
+      {...baseOptions()}
+      tree={source.getPageTree()}
+      tabs={{
+        transform: (option, node) => ({
+          ...option,
+          icon: (
+            <div
+              className="size-full [&_svg]:size-full"
+              style={{
+                color: option.url === "/draft" ? "#a78bfa" : "#60a5fa",
+              }}
+            >
+              {node.icon}
+            </div>
+          ),
+        }),
+      }}
+    >
       {children}
     </DocsLayout>
   )
